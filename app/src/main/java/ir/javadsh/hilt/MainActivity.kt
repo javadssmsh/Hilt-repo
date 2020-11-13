067336ab9ee8f26af70999110e250b9c698dc01c
javad <j.shirkhani@clickhub.ir>
2020-11-13 22:31:32 +0330

    constructor injection

--- app/src/main/java/ir/javadsh/hilt/MainActivity.kt
+++ app/src/main/java/ir/javadsh/hilt/MainActivity.kt
@@ -2,11 +2,38 @@ package ir.javadsh.hilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    //field injection
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        println(someClass.doAThing())
+        println(someClass.doSomeOtherThing())
     }
 }
+
+
+class SomeClass
+@Inject
+constructor(
+    private val someOtherClass: SomeOtherClass
+) {
+    fun doAThing(): String {
+        return "Look I did a thing !"
+    }
+
+    fun doSomeOtherThing(): String {
+        return someOtherClass.doSomeOtherThing()
+    }
+}
+
+
