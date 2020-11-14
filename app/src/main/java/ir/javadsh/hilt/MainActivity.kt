f62c2ec4a6ce0adea34df369be3f23c13a12bc7a
javad <j.shirkhani@clickhub.ir>
2020-11-14 22:56:59 +0330

    senarion one

--- app/src/main/java/ir/javadsh/hilt/MainActivity.kt
+++ app/src/main/java/ir/javadsh/hilt/MainActivity.kt
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -35,8 +40,7 @@ class MyFragment : Fragment() {
 class SomeClass
 @Inject
 constructor(
-    //private val someInterfaceImplementation: SomeInterface
-    private val gson: Gson
+    private val someInterfaceImplementation: SomeInterface
 ) {
     fun doAThing(): String {
         //return "Look I got : ${someInterfaceImplementation.getString()}"
@@ -55,6 +59,24 @@ constructor() : SomeInterface {
 
 interface SomeInterface {
     fun getString(): String
+}
+
+@InstallIn(ActivityComponent::class)
+@Module
+abstract class MyModule() {
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindSomeDependency(
+        someImp: SomeInterfaceImplementation
+    ): SomeInterface
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindGson(
+        gSon: Gson
+    ): Gson
+
 }
 
 
