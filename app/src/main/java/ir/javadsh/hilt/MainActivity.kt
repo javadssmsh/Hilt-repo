60fa186e6299b0823204aa5899c0bbcac1c4dfcd
javad <j.shirkhani@clickhub.ir>
2020-11-15 01:40:11 +0330

    Providing Instances of the Same Type

--- app/src/main/java/ir/javadsh/hilt/MainActivity.kt
+++ app/src/main/java/ir/javadsh/hilt/MainActivity.kt
@@ -11,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @AndroidEntryPoint
@@ -24,7 +25,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println(someClass.doAThing())
+        println(someClass.doAThing1())
+        println(someClass.doAThing2())
     }
 }
 
@@ -39,22 +41,35 @@ class MyFragment : Fragment() {
 class SomeClass
 @Inject
 constructor(
-    private val someInterfaceImplementation: SomeInterface
+    @MyModule.Impl1 private val someInterfaceImpl1: SomeInterface,
+    @MyModule.Impl2  private val someInterfaceImpl2: SomeInterface
 ) {
-    fun doAThing(): String {
-        //return "Look I got : ${someInterfaceImplementation.getString()}"
-        return "someThing"
+    fun doAThing1(): String {
+        return "Look I got : ${someInterfaceImpl1.getString()}"
+        //return "someThing"
+    }
+
+    fun doAThing2(): String {
+        return "Look I got : ${someInterfaceImpl2.getString()}"
+        //return "someThing"
     }
 }
 
-class SomeInterfaceImpl
+class SomeInterfaceImpl1
 @Inject
 constructor(
-    private val someDependency: String
 ) : SomeInterface {
-
     override fun getString(): String {
-        return "A Thing !"
+        return "A Thing 1!"
+    }
+}
+
+class SomeInterfaceImpl2
+@Inject
+constructor(
+) : SomeInterface {
+    override fun getString(): String {
+        return "A Thing 2!"
     }
 }
 
@@ -66,26 +81,29 @@ interface SomeInterface {
 @Module
 class MyModule() {
 
+    @Impl1
     @Singleton
     @Provides
-    fun provideSomeString(): String {
-        return "some string "
-    }
-
-    @Singleton
-    @Provides
-    fun provideSomeInterface(
-        someString: String
+    fun provideSomeInterface1(
     ): SomeInterface {
-        return SomeInterfaceImpl(someString)
+        return SomeInterfaceImpl1()
     }
 
+    @Impl2
     @Singleton
     @Provides
-    fun provideGson(): Gson {
-        return Gson()
+    fun provideSomeInterface2(
+    ): SomeInterface {
+        return SomeInterfaceImpl2()
     }
 
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class Impl1
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class Impl2
 
 }
 
