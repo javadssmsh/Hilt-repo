91f9f951807a111d7bf3e762db439c6f5bcb210f
javad <j.shirkhani@clickhub.ir>
2020-11-25 17:27:25 +0330

    Create BlogDatabase.kt

---
+++ app/src/main/java/ir/javadsh/hilt/room/BlogDatabase.kt
@@ -0,0 +1,10 @@
+package ir.javadsh.hilt.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [BlogCacheEntity::class], version = 1)
+abstract class BlogDatabase : RoomDatabase() {
+
+    //abstract fun blogDao(): BlogDao
+}
