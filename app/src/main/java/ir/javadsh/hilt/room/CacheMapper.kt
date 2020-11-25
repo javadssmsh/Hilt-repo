6833613d54736ba7d59d87f2322b98446821e616
javad <j.shirkhani@clickhub.ir>
2020-11-25 17:27:30 +0330

    Create CacheMapper.kt

---
+++ app/src/main/java/ir/javadsh/hilt/room/CacheMapper.kt
@@ -0,0 +1,35 @@
+package ir.javadsh.hilt.room
+
+import ir.javadsh.hilt.model.Blog
+import ir.javadsh.hilt.utils.EntityMapper
+import org.w3c.dom.Entity
+import javax.inject.Inject
+
+class CacheMapper
+@Inject
+constructor() : EntityMapper<BlogCacheEntity, Blog> {
+    override fun mapFromEntity(entity: BlogCacheEntity): Blog {
+        return Blog(
+            id = entity.id,
+            title = entity.title,
+            body = entity.body,
+            image = entity.image,
+            category = entity.category
+        )
+    }
+
+    override fun mapToEntity(domainModel: Blog): BlogCacheEntity {
+        return BlogCacheEntity(
+            id = domainModel.id,
+            title = domainModel.title,
+            body = domainModel.body,
+            image = domainModel.image,
+            category = domainModel.category
+        )
+    }
+
+    fun mapFromEntityList(entities: List<BlogCacheEntity>): List<Blog> {
+        return entities.map { mapFromEntity(it) }
+    }
+
+}
