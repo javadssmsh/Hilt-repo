188f7b9b6c7aa0c23df2ad5a79675c7056d750d7
javad <j.shirkhani@clickhub.ir>
2020-11-25 17:27:09 +0330

    Create BlogNetworkEntity.kt

---
+++ app/src/main/java/ir/javadsh/hilt/retrofit/BlogNetworkEntity.kt
@@ -0,0 +1,30 @@
+package ir.javadsh.hilt.retrofit
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class BlogNetworkEntity(
+    @SerializedName("pk")
+    @Expose
+    var id: Int,
+
+    @SerializedName("title")
+    @Expose
+    var title: String,
+
+    @SerializedName("body")
+    @Expose
+    var body: String,
+
+    @SerializedName("image")
+    @Expose
+    var image: String,
+
+    @SerializedName("category")
+    @Expose
+    var category: String,
+
+    ) {
+
+
+}
