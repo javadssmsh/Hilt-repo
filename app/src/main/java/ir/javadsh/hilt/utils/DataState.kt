83c3e8a0933d55aeb47fac2823851e5f12c8b251
javad <j.shirkhani@clickhub.ir>
2020-11-27 18:49:15 +0330

    Update DataState.kt

--- app/src/main/java/ir/javadsh/hilt/utils/DataState.kt
+++ app/src/main/java/ir/javadsh/hilt/utils/DataState.kt
@@ -1,4 +1,10 @@
 package ir.javadsh.hilt.utils
 
-class DataState {
+sealed class DataState<out R> {
+
+    data class Success<out T>(val data: T) : DataState<T>()
+
+    data class Error(val exception: Exception) : DataState<Nothing>()
+
+    object Loading : DataState<Nothing>()
 }
